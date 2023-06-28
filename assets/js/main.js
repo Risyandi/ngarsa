c35d08bd32d06dea177bab104457337b7a9aaffe
Risyandi <risyandi.94@gmail.com>
2023-06-28 14:47:39 +0700

    fix: updated switch language

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,14 +162,43 @@ const bannerDesc = document.getElementById('banner-desc'),
     formSend = document.getElementById('form-send'),
     formMandatory = document.getElementById('mandatory'),
     aboutDesc1 = document.getElementById('about-desc1'),
-    aboutDesc2= document.getElementById('about-desc2'),
+    aboutDesc2 = document.getElementById('about-desc2'),
     aboutDesc3 = document.getElementById('about-desc3'),
     aboutDesc4 = document.getElementById('about-desc4'),
     serviceDesc1 = document.getElementById('service-desc1'),
-    serviceDesc2 = document.getElementById('service-desc2');
+    serviceDesc2 = document.getElementById('service-desc2'),
+    serviceItemTitle1 = document.getElementById('service-item-title1'),
+    serviceItemDesc1 = document.getElementById('service-item-desc1'),
+    serviceItemTitle2 = document.getElementById('service-item-title2'),
+    serviceItemDesc2 = document.getElementById('service-item-desc2'),
+    serviceItemTitle3 = document.getElementById('service-item-title3'),
+    serviceItemDesc3 = document.getElementById('service-item-desc3'),
+    serviceItemTitle4 = document.getElementById('service-item-title4'),
+    serviceItemDesc4 = document.getElementById('service-item-desc4'),
+    whyUsTitle = document.getElementById('whyus-title'),
+    whyUsSubTitle1 = document.getElementById('whyus-sub-title1'),
+    whyUsSubDesc1 = document.getElementById('whyus-sub-desc1'),
+    whyUsSubTitle2 = document.getElementById('whyus-sub-title2'),
+    whyUsSubDesc2 = document.getElementById('whyus-sub-desc2'),
+    whyUsSubTitle3 = document.getElementById('whyus-sub-title3'),
+    whyUsSubDesc3 = document.getElementById('whyus-sub-desc3'),
+    footerOurServiceItem1 = document.getElementById('footer-ourservice-item1'),
+    footerOurServiceItem2 = document.getElementById('footer-ourservice-item2'),
+    footerOurServiceItem3 = document.getElementById('footer-ourservice-item3'),
+    footerOurServiceItem4 = document.getElementById('footer-ourservice-item4'),
+    menu1 = document.getElementById('menu1'),
+    menu2 = document.getElementById('menu2'),
+    menu3 = document.getElementById('menu3'),
+    menu4 = document.getElementById('menu4');
 
 const language = {
     id: {
+        menu: {
+            menu1: "Beranda",
+            menu2: "Layanan",
+            menu3: "Tentang",
+            menu4: "Kontak",
+        },
         banner: {
             desc: "Di ngarsa digital kami membuat dengan kualitas untuk mendapatkan keunggulan dari sesuatu yang kami lakukan, dan kami memberikan solusi untuk memecahkan masalah Anda.",
         },
@@ -184,52 +213,58 @@ const language = {
             desc2: "Kami dapat membantu Anda mewujudkan proyek digital Anda seperti website company profile, desain grafis, desain web, fotografi, dan mendukung kinerja bisnis Anda menggunakan teknologi Marketing.",
             item: [
                 {
-                    title: "Web & Creative",
+                    title: "Web & Kreatif",
                     desc: "Kami membuat desain web dan interaktif menjadi sederhana dan ikonik.",
                 },
                 {
-                    title: "Marketing Technology",
+                    title: "Teknologi Pemasaran",
                     desc: "Kami memberikan solusi untuk menyampaikan kampanye dengan Teknologi Pemasaran yang jelas dan unik.",
                 },
                 {
-                    title: "SEO & Analytics",
+                    title: "SEO & Analitik",
                     desc: "Kami mempercepat halaman Anda untuk ditemukan dalam pencarian.",
                 },
                 {
-                    title: "Design & Branding",
+                    title: "Desain & Merek",
                     desc: "Kami membuat desain yang simpel, elegan dan ikonik. untuk merek Anda.",
                 }
             ],
         },
         whyus:
         {
-            title: "Why Us",
+            title: "Mengapa Kami",
             sub: [{
-                title: "Quality Results",
-                desc: "At ngarsa digital, we provide the best quality according to what you need.",
+                title: "Hasil Berkualitas",
+                desc: "Di ngarsa digital, kami memberikan kualitas terbaik sesuai dengan yang anda butuhkan.",
             },
             {
-                title: "Affordable Pricing",
-                desc: "At ngarsa digital, we provide relatively cheap and high-quality prices.",
+                title: "Harga Terjangkau",
+                desc: "Di ngarsa digital kami memberikan harga yang relatif murah dan berkualitas.",
             },
             {
-                title: "Free Support & Consultation",
-                desc: "At ngarsa digital, we support and provide consultation about creative digital and marketing communications strategy.",
+                title: "Dukungan & Konsultasi Gratis",
+                desc: "Di ngarsa digital, kami mendukung dan memberikan konsultasi tentang digital kreatif dan strategi komunikasi pemasaran.",
             }]
         },
-        send:'Kirim',
+        send: 'Kirim',
         contact: { title: "Kontak Kami", name: "Nama Lengkap *", message: "Pesan *", email: "Alamat Surel *" },
         partner: 'Mitra',
         followus: "Ikuti Kami",
         ourservices: "Layanan Kami",
         placeholder: {
             name: "Masukkan Nama Lengkap Disini",
-            email:"Masukkan Alamat Surel Disini",
+            email: "Masukkan Alamat Surel Disini",
             message: "Masukkan Pesan Disini",
             mandatory: "(*) Wajib di isi"
         }
     },
     en: {
+        menu: {
+            menu1: "Home",
+            menu2: "Services",
+            menu3: "About",
+            menu4: "Contact",
+        },
         banner: {
             desc: "At ngarsa digital we made with quality for getting excellence of something we do, and we provide solutions to solve your problems.",
         },
@@ -278,18 +313,18 @@ const language = {
             }]
         },
         send: "Send",
-        contact: { 
-            title: "Contact Us", 
-            name: "Full Name *", 
-            message: "Message *", 
-            email: "Email *" 
+        contact: {
+            title: "Contact Us",
+            name: "Full Name *",
+            message: "Message *",
+            email: "Email *"
         },
-        partner:"Partner",
+        partner: "Partner",
         followus: "Follow Us",
         ourservices: "Our Services",
         placeholder: {
             name: "Enter your full name here",
-            email:"Enter your email here",
+            email: "Enter your email here",
             message: "Enter your message here",
             mandatory: "(*) required to filled"
         }
@@ -318,6 +353,29 @@ const changesLanguage = function (lang) {
         aboutDesc4.textContent = language.en.about.desc4;
         serviceDesc1.textContent = language.en.services.desc1;
         serviceDesc2.textContent = language.en.services.desc2;
+        serviceItemTitle1.textContent = language.en.services.item[0].title;
+        serviceItemDesc1.textContent = language.en.services.item[0].desc;
+        serviceItemTitle2.textContent = language.en.services.item[1].title;
+        serviceItemDesc2.textContent = language.en.services.item[1].desc;
+        serviceItemTitle3.textContent = language.en.services.item[2].title;
+        serviceItemDesc3.textContent = language.en.services.item[2].desc;
+        serviceItemTitle4.textContent = language.en.services.item[3].title;
+        serviceItemDesc4.textContent = language.en.services.item[3].desc;
+        whyUsTitle.textContent = language.en.whyus.title;
+        whyUsSubTitle1.textContent = language.en.whyus.sub[0].title;
+        whyUsSubDesc1.textContent = language.en.whyus.sub[0].desc;
+        whyUsSubTitle2.textContent = language.en.whyus.sub[1].title;
+        whyUsSubDesc2.textContent = language.en.whyus.sub[1].desc;
+        whyUsSubTitle3.textContent = language.en.whyus.sub[2].title;
+        whyUsSubDesc3.textContent = language.en.whyus.sub[2].desc;
+        footerOurServiceItem1.textContent = language.en.services.item[0].title;
+        footerOurServiceItem2.textContent = language.en.services.item[1].title;
+        footerOurServiceItem3.textContent = language.en.services.item[2].title;
+        footerOurServiceItem4.textContent = language.en.services.item[3].title;
+        menu1.textContent = language.en.menu.menu1;
+        menu2.textContent = language.en.menu.menu2;
+        menu3.textContent = language.en.menu.menu3;
+        menu4.textContent = language.en.menu.menu4;
     } else {
         bannerDesc.textContent = language.id.banner.desc;
         bannerDescContact.textContent = language.id.contact.title;
@@ -339,5 +397,28 @@ const changesLanguage = function (lang) {
         aboutDesc4.textContent = language.id.about.desc4;
         serviceDesc1.textContent = language.id.services.desc1;
         serviceDesc2.textContent = language.id.services.desc2;
+        serviceItemTitle1.textContent = language.id.services.item[0].title;
+        serviceItemDesc1.textContent = language.id.services.item[0].desc;
+        serviceItemTitle2.textContent = language.id.services.item[1].title;
+        serviceItemDesc2.textContent = language.id.services.item[1].desc;
+        serviceItemTitle3.textContent = language.id.services.item[2].title;
+        serviceItemDesc3.textContent = language.id.services.item[2].desc;
+        serviceItemTitle4.textContent = language.id.services.item[3].title;
+        serviceItemDesc4.textContent = language.id.services.item[3].desc;
+        whyUsTitle.textContent = language.id.whyus.title;
+        whyUsSubTitle1.textContent = language.id.whyus.sub[0].title;
+        whyUsSubDesc1.textContent = language.id.whyus.sub[0].desc;
+        whyUsSubTitle2.textContent = language.id.whyus.sub[1].title;
+        whyUsSubDesc2.textContent = language.id.whyus.sub[1].desc;
+        whyUsSubTitle3.textContent = language.id.whyus.sub[2].title;
+        whyUsSubDesc3.textContent = language.id.whyus.sub[2].desc;
+        footerOurServiceItem1.textContent = language.id.services.item[0].title;
+        footerOurServiceItem2.textContent = language.id.services.item[1].title;
+        footerOurServiceItem3.textContent = language.id.services.item[2].title;
+        footerOurServiceItem4.textContent = language.id.services.item[3].title;
+        menu1.textContent = language.id.menu.menu1;
+        menu2.textContent = language.id.menu.menu2;
+        menu3.textContent = language.id.menu.menu3;
+        menu4.textContent = language.id.menu.menu4;
     }
 }
